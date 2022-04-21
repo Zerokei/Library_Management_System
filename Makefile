25d9d6d7cfb97e996be7ad6b32c22ad86729c24d
Zerokei <twentyeightkei@gmail.com>
2022-04-21 13:41:20 +0800

    feat: save

--- Makefile
+++
@@ -1,5 +0,0 @@
-PYTHON=/usr/bin/python3
-all: test
-test:
-	${PYTHON} -m gui.py
-	python3 create_table.py
--- db/db.py
+++ db/db.py
@@ -1,9 +1,17 @@
 import pymysql
 
-db = pymysql.connect(
-    host = '127.0.0.1',
-    port = 3306,
-    user = 'zerokei',
-    passwd = '123456',
-    db='Library_management_system',
-    charset = 'utf8')
+class DB:
+    def __init__(self):
+        self.connect = pymysql.connect(
+            host = '127.0.0.1',
+            port = 3306,
+            user = 'zerokei',
+            passwd = '123456',
+            db='Library_management_system',
+            charset = 'utf8')
+    def query_sql(self, sql):
+        self.connect.cursor().execute(sql)
+        return self.connect.cursor().fetchall()
+    def update_sql(self, sql):
+        self.connect.cursor().execute(sql)
+        self.connect.commit()
